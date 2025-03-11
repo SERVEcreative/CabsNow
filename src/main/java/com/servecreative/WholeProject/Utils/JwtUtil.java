3b1c39d33d883aa9a766b58f840a70eb72d951af
Rahul kumar <rahul.nitjsr67@gmail.com>
2025-03-12 01:22:25 +0530

    added new Utils for driver registration handling

---
+++ src/main/java/com/servecreative/WholeProject/Utils/JwtUtil.java
@@ -0,0 +1,24 @@
+package com.servecreative.WholeProject.Utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+
+    public String generateToken(String phoneNumber) {
+        return Jwts.builder()
+                .setSubject(phoneNumber)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
