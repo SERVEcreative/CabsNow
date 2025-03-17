52c71a367ba108cc3ac71bb0e3f49656a6be4094
Rahul kumar <rahul.nitjsr67@gmail.com>
2025-03-17 17:03:25 +0530

    added updated ridercontroller file

--- src/main/java/com/servecreative/WholeProject/Controller/RiderController.java
+++ src/main/java/com/servecreative/WholeProject/Controller/RiderController.java
@@ -3,6 +3,8 @@ package com.servecreative.WholeProject.Controller;
 import com.servecreative.WholeProject.Model.Duty;
 import com.servecreative.WholeProject.Services.RiderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,13 +19,28 @@ public class RiderController {
     @PostMapping("/book")
     public Duty bookRide(@RequestParam int riderId,
                          @RequestParam String pickupLocation,
-                         @RequestParam String dropLocation) {
-        return riderService.bookRide(riderId, pickupLocation, dropLocation);
+                         @RequestParam String dropLocation,
+                         @RequestParam String vehicleType) {
+        return riderService.bookRide(riderId, pickupLocation, dropLocation,vehicleType);
     }
     //cancel a ride
-    @PutMapping("/cancelduties/{riderId}")
+    @PutMapping("/cancelDuties/{riderId}")
     public ResponseEntity<Duty> cancelRide(@PathVariable int riderId) {
         Duty updatedDuty = riderService.cancelRide(riderId);
         return ResponseEntity.ok(updatedDuty);
     }
+    @GetMapping("/history/pdf/{riderId}")
+    public ResponseEntity<byte[]> downloadDutyHistoryPdf(@PathVariable int riderId) {
+        byte[] pdfBytes = riderService.generateDutyHistoryPdf(riderId);
+
+        if (pdfBytes == null) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=Duty_History.pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(pdfBytes);
+    }
+
 }
