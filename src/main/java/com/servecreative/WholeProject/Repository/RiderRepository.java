b63215a62bd0fdd2cc7da50ffc1ff37c0b6e8b62
Rahul kumar <rahul.nitjsr67@gmail.com>
2025-03-31 00:48:08 +0530

    added new file

--- src/main/java/com/servecreative/WholeProject/Repository/RiderRepository.java
+++ src/main/java/com/servecreative/WholeProject/Repository/RiderRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface RiderRepository extends JpaRepository<Rider,Integer> {
+public interface RiderRepository extends JpaRepository<Rider, Integer> {
 
-    Optional<Rider> findById(Integer riderId);
+    Optional<Rider> findById(int riderId);
 }
